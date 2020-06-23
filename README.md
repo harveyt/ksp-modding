5701f6e8b5bd2c9c83bc362be480ef8c99e82bf2
Harvey Thompson <harv.thompson@icloud.com>
2020-06-22 21:31:24 -0700

    Added ckan installation.

--- README.md
+++ README.md
@@ -11,3 +11,12 @@ Much of this is automated using brew and other tools.
 * Ensures the right version of Unity is installed
 * Ensures other KSP tools are installed
 * Ensures Emacs development tools are installed
+
+References
+==========
+
+* [Wiki Tutorials](https://wiki.kerbalspaceprogram.com/wiki/Category:Modding_Tutorials)
+* [Forum Add-on Development](https://forum.kerbalspaceprogram.com/index.php?/forum/36-add-on-development/)
+* [Modders 1.9.0 Notes](https://forum.kerbalspaceprogram.com/index.php?/topic/191584-modders-notes-190/)
+* [Modders 1.8.0 Notes](https://forum.kerbalspaceprogram.com/index.php?/topic/188933-180-modders-notes/)
+* [Installing CKAN on macOS](https://github.com/KSP-CKAN/CKAN/wiki/Installing-CKAN-on-macOS)
--- provision.sh
+++ provision.sh
@@ -16,6 +16,10 @@ PISH=$PISH_ROOT/bin/pish
 
 SUDO_ROOT_ITEM="MacBookPro Harvey"
 
+STEAMAPPS="$HOME/Library/Application Support/Steam/steamapps/common"
+KSP="$STEAMAPPS/Kerbal Space Program"
+MONO_PATHS=/etc/paths.d/mono-commands
+
 # ================================================================================
 # Main
 #
@@ -25,6 +29,17 @@ function converge_defaults()
     requirement 1pass_logged_in
 
     requirement brew_cask_install steam
+
+    # TODO: Use steamcmd or steam.exe to install? Automation only goes so far...
+    requirement directory_exists "$KSP"
+
+    requirement brew_install mono
+    requirement brew_install ckan
+    requirement brew_cask_install ckan
+
+    if [[ -f $MONO_PATHS ]]; then
+	export PATH=$PATH:$(cat $MONO_PATHS)
+    fi
 }
 
 converge "$@"
